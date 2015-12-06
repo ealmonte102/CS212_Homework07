a2b95fe942bc099f093c6efaa82754374664cd3c
Evan Almonte <ealmonte102@qc.cuny.edu>
2015-12-06 15:46:14 -0500

    Adds class MyException with a default and parameterized constructor

---
+++ src/MyException.java
@@ -0,0 +1,11 @@
+/**
+ * Homework #7 Created by Evan Almonte on 12/6/2015.
+ */
+public class MyException extends Exception {
+    MyException() {
+        super();
+    }
+    MyException(String exceptionString) {
+        super(exceptionString);
+    }
+}
